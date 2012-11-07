c398c5f8c7bd6f8a686b86604bad8982e2f3bdc5
Kevin Turner <keturn@keturn.net>
2012-11-07 11:37:42 -0800

    code scouts is two words

--- code_scouts_platform/urls.py
+++ code_scouts_platform/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'codescouts_platform.views.home', name='home'),
-    # url(r'^codescouts_platform/', include('codescouts_platform.foo.urls')),
+    # url(r'^$', 'code_scouts_platform.views.home', name='home'),
+    # url(r'^code_scouts_platform/', include('code_scouts_platform.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
