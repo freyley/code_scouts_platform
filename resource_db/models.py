bace62b668b338075047d8f725a90eab1bf80930
Kevin Turner <kevin@codescouts.org>
2013-02-20 19:24:56 -0800

    implementing importFromCSV

--- resource_db/models.py
+++ resource_db/models.py
@@ -1,4 +1,6 @@
+import csv
 from django.db import models
+
 
 class LearningResource(models.Model):
     url = models.URLField(max_length=2047)
@@ -7,3 +9,27 @@ class LearningResource(models.Model):
     description = models.TextField()
     cost = models.CharField(max_length=255)
     content_format = models.CharField(max_length=511, blank=True)
+
+
+def importFromCSV(input_file):
+    """Create LearningResources from a CSV file.
+
+    Note: This does not save the results!
+
+    :type input_file: file
+    :rtype: list of LearningResource
+    """
+    reader = csv.reader(input_file)
+    reader.next() # skip headers
+    resources = []
+    for record in reader:
+        new_resource = LearningResource(
+            url=record[0],
+            title=record[1],
+            tags=record[3],
+            description=record[4],
+            cost=record[5],
+            content_format=record[9]
+        )
+        resources.append(new_resource)
+    return resources
--- resource_db/tests.py
+++ resource_db/tests.py
@@ -1,16 +1,18 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
-
+from resource_db.models import importFromCSV
+from cStringIO import StringIO
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_importFromCSV(self):
+        input_file = StringIO("""URL,Short Title,Part of larger resource?,tags,description,cost,Location (or online),Start date,End date,"Format (video, tutorial, book etc.)",Members who might start here
+http://example.com,Example Title,No,"Some, Tags",example description,$99,Online,N/A,N/A,Webpage,
+""")
+        resources = importFromCSV(input_file)
+        self.assertEqual(len(resources), 1)
+        r = resources[0]
+        self.assertEqual(r.url, 'http://example.com')
+        self.assertEqual(r.title, 'Example Title')
+        self.assertEqual(r.tags, 'Some, Tags')
+        self.assertEqual(r.description, 'example description')
+        self.assertEqual(r.cost, '$99')
+        self.assertEqual(r.content_format, 'Webpage')
