f7f9c4e39ec91a0a2ad9c61510674f72e485dd64
Kevin Turner <kevin@codescouts.org>
2013-03-06 14:32:03 -0800

    events.urls: add urlconf to CalendarView

---
+++ events/tests/test_urls.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Test resource_db.urls.
+"""
+
+from unittest import TestCase
+from django.core.urlresolvers import resolve
+
+from .. import urls
+from .. import views
+
+
+class TestURLs(TestCase):
+    def test_resourceList(self):
+        match = resolve('/calendar', urls)
+        self.assertEqual(match.func, views.CalendarView)
