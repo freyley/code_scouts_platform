95ca1f404edd48d3ebf199ff183b327d533ef0c3
Laura Uva <Ldu45@hotmail.com>
2013-03-20 18:57:16 -0700

    Change function names according to PEP8

--- events/tests/test_views.py
+++ events/tests/test_views.py
@@ -4,10 +4,10 @@ from django.test import TestCase
 
 
 class CalendarViewTest(TestCase):
-    def test_calendarView(self):
+    def test_calendar_view(self):
         view = self.client.get('/events/calendar')
         self.assertContains(view, 'Calendar', status_code=200)
 
-    def test_calendarTemplate(self):
+    def test_calendar_template(self):
         view = self.client.get('/events/calendar')
         self.assertTemplateUsed(view, 'events/calendar.html')
