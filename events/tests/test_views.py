d4310483c21f644591fc8e6ce7a6a185d78a52fe
Kevin Turner <kevin@codescouts.org>
2013-03-27 17:25:35 -0700

    test_view: invoke view directly instead of using the test client

--- events/tests/test_views.py
+++ events/tests/test_views.py
@@ -4,13 +4,12 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.conf import settings
 
+from ..views import calendar_view
+
 class CalendarViewTest(TestCase):
 
     def test_calendar_view(self):
-        view = self.client.get(reverse('events:calendar'))
-        self.assertContains(view, 'Calendar', status_code=200)
-        self.assertContains(view, settings.CS_GOOGLE_CALENDAR)
-
-    def test_calendar_template(self):
-        view = self.client.get(reverse('events:calendar'))
-        self.assertTemplateUsed(view, 'events/calendar.html')
+        response = calendar_view("FakeRequest")
+        self.assertContains(response, settings.CS_GOOGLE_CALENDAR,
+            status_code=200)
+        self.assertTemplateUsed(response, 'events/calendar.html')
