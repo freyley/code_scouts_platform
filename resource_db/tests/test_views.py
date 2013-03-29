dfb63466c2db308add5ac97d08c6506bd11053bd
Laura Uva <Ldu45@hotmail.com>
2013-03-29 16:10:30 -0700

    update to resources view test

--- resource_db/tests/test_views.py
+++ resource_db/tests/test_views.py
@@ -4,15 +4,16 @@ from resource_db.models import LearningResource
 class ResourceDBTest(TestCase):
 	def setUp(self):
 		LearningResource.objects.get_or_create(
-			url='www.udacity.com', title='Online course', tags='web dev', 
+			url='www.udacity.com', title='New Title', tags='web dev', 
 			description='Online web development course', cost='Free', content_format='online class')
 		LearningResource.objects.get_or_create(
-			url='www.udemy.com', title='Online course', tags='python, web dev',
+			url='www.udemy.com', title='Second Test Title', tags='python, web dev',
 			description='Online Python course', cost='$12', content_format='lectures')
 		self.view = self.client.get('/resources/')
 
 	def test_resource_list(self):
-		self.assertContains(self.view, 'Online course', count=2)
+		self.assertContains(self.view, 'New Title')
+		self.assertContains(self.view, 'Second Test Title')
 
 	def test_resource_db_view(self):
 		self.assertContains(self.view, 'Resource List', status_code=200)
