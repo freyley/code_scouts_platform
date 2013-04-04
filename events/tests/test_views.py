f498aed63dfdcdeffa1394cc2eb26aabbdac1c00
Kevin Turner <kevin@codescouts.org>
2013-04-03 17:48:49 -0700

    events.views: add login_required. (yes, this redirects to a login page that doesn't exist yet.)

--- events/tests/test_views.py
+++ events/tests/test_views.py
@@ -1,15 +1,29 @@
 # -*- coding: utf-8 -*-
-from django.core.urlresolvers import reverse
+from django.contrib.auth.models import AnonymousUser, User
 
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from django.conf import settings
 
 from ..views import calendar_view
 
 class CalendarViewTest(TestCase):
 
-    def test_calendar_view(self):
-        response = calendar_view("FakeRequest")
+    def setUp(self):
+        self.request = RequestFactory().request()
+
+
+    def test_cannot_view_without_login(self):
+        self.request.user = AnonymousUser()
+        response = calendar_view(self.request)
+        # Response should *not* contain Calendar, should do Redirect.
+        # assertRedirect doesn't work with RequestFactory
+        self.assertNotContains(response, "Calendar", 302)
+        self.assertEqual(response['Location'], "/accounts/login/?next=/")
+
+
+    def test_calendar_view_with_login(self):
+        self.request.user = User()
+        response = calendar_view(self.request)
         self.assertContains(response, settings.CS_GOOGLE_CALENDAR,
             status_code=200)
         self.assertTemplateUsed(response, 'events/calendar.html')
--- events/views.py
+++ events/views.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import render
 from django.conf import settings
 
+@login_required
 def calendar_view(request):
     return render(request, 'events/calendar.html', {
         'calendar_id': settings.CS_GOOGLE_CALENDAR
