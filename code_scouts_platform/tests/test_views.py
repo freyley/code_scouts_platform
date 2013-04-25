06aad4302c166a6fcaf99017d34d6e0873d3fee5
Kevin Turner <kevin@codescouts.org>
2013-04-24 18:43:12 -0700

    views.login: pass through the next-page information for redirect

--- code_scouts_platform/tests/test_views.py
+++ code_scouts_platform/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.test import TestCase, RequestFactory
 from code_scouts_platform.views import login
 
@@ -10,3 +11,10 @@ class LoginViewTest(TestCase):
         self.assertTemplateUsed(response, "code_scouts_platform/login.html")
         self.assertContains(response, "Persona", status_code=200)
 
+
+    def test_login_page_includes_redirect_target(self):
+        redirect_to = '/redirect_to_here'
+        self.request = RequestFactory().get('/login',
+            {REDIRECT_FIELD_NAME: redirect_to})
+        response = login(self.request)
+        self.assertContains(response, redirect_to)
--- code_scouts_platform/views.py
+++ code_scouts_platform/views.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 """Project-level views for the Code Scouts Platform."""
+from django.contrib.auth import REDIRECT_FIELD_NAME
 from django.shortcuts import render
 
 
 def login(request):
-    return render(request, 'code_scouts_platform/login.html')
+    return render(request, 'code_scouts_platform/login.html', {
+        'redirect_to': request.REQUEST.get(REDIRECT_FIELD_NAME, ''),
+        'REDIRECT_FIELD_NAME': REDIRECT_FIELD_NAME,
+    })
